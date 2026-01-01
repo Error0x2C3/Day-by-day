7380fa5560e55006e6228f4cfcd36922fcfc9afc
Error0x2C3 <104620425+Error0x2C3@users.noreply.github.com>
2026-01-01 02:10:15 +0100

    ÃExo java

---
+++ day-001/ex02/Task.java
@@ -0,0 +1,15 @@
+package labo15.ex02;
+
+public class Task {
+
+    private final String name;
+
+    public Task(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+}
