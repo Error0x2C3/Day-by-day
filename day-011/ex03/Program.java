c5bc78efcb2d9285c3bf8e911af21d7e6f81f9df
Error0x2C3 <104620425+Error0x2C3@users.noreply.github.com>
2026-01-11 23:49:45 +0100

    java

---
+++ day-011/ex03/Program.java
@@ -0,0 +1,15 @@
+package labo16.ex03;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.util.Scanner;
+
+public class Program {
+    public static void main(String args[]) {
+
+    }
+    public static void copy_first_in_second(File file_one,File file_second){
+        
+    }
+}
