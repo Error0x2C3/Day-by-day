194e77e011b9723f777a6f9fc7103c035268ce54
Error0x2C3 <104620425+Error0x2C3@users.noreply.github.com>
2026-02-26 20:20:07 +0100

    JavaFx Chap 3 ex3 complet

---
+++ day-046/Chap3/Ex-Chap3-03-counter_00_bootstrap/00_counter_bootstrap/src/main/java/model/CounterModel.java
@@ -0,0 +1,62 @@
+package model;
+
+import java.util.Observable;
+
+/*
+Le modèle étant responsable du respect des règles-métier,
+il doit toujours être dans un état acceptable.
+Donc, une modification entraînant une violation de ces règles
+devra déclencher une RuntimeException
+(voir l'exemple de la classe Date dans le cours de Programmation Orientée Objet).
+ */
+public class CounterModel extends Observable {
+
+    private int nbr ;
+    private final CounterChangeType counter_change_type;
+    public CounterModel(){
+        this.nbr = (int)(Math.random() * (3 - (-3) + 1)) + (-3); // Chiffre au hasard entre -3 et 3 compris.
+        this.counter_change_type = CounterChangeType.VALUE_CHANGED;
+    }
+
+
+    public int get_nbr() {
+        return this.nbr;
+    }
+    public String get_string_nbr() {
+        return String.valueOf(nbr);
+    }
+    public CounterChangeType get_counter_change_type(){return this.counter_change_type;}
+    // Gestion règles métiers pour le nombre.
+    // ---------------------------------------------------
+    public void set_nbr(int number) {
+        if(number < -3 || number > 3){
+            throw new RuntimeException("Le nombre donné n'est pas compris entre -3 et 3 compris.");
+        }
+        this.nbr = number;
+        this.setChanged();
+        this.notifyObservers(this.get_counter_change_type());
+        /*
+        La méthode notifyObservers() appelle la méthode udpate contenue dans chaque classe observateur.
+        Il se passe en paramètre dans la méthode update d'ou le Observable o comme argument dans cette méthode.
+        + peut mettre un paramètre en plus dans update ex :
+        this.notifyObservers(arg: this) correspond à l'argument Object arg de la méthode update.
+         */
+    }
+    // ---------------------------------------------------
+
+    // Gestion règles métiers pour les boutons.
+    // ---------------------------------------------------
+    public boolean btn_less_must_be_disabled(){
+        if(this.get_nbr() <= -3){
+            return true;
+        }
+        return false;
+    }
+    public boolean btn_add_must_disabled(){
+        if(this.get_nbr() >= 3){
+            return true;
+        }
+        return false;
+    }
+    // ---------------------------------------------------
+}
