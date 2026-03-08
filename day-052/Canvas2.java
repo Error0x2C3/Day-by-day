bc79622cd9d3e4080f3418e56af8763035494c8d
Error0x2C3 <104620425+Error0x2C3@users.noreply.github.com>
2026-03-09 00:38:18 +0100

    projet JavaFx model Canvas

---
+++ day-052/Canvas2.java
@@ -0,0 +1,43 @@
+package scratch.model;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+
+public class Canvas { // Liste de droites tracée par la tortue.
+
+    // L'ObservableList va prévenir l'interface graphique à chaque nouveau trait.
+    private final ObservableList<Segment> segments = FXCollections.observableArrayList();
+
+    public Canvas() {
+        // Le constructeur n'a rien de spécial à faire.
+    }
+
+    /*
+    On utilise la méthode statique unmodifiableObservableList pour "emballer" segments dans une liste non modifiable
+    Remarque : comme le type de retour est ObservableList (il n'existe pas de ReadOnlyObservableList dans la librairie),
+    si le client essaie de modifier la liste (ajout, suppression, ...), il y aura une Exception à l'exécution.
+   */
+    public ObservableList<Segment> getSegments() {
+        return FXCollections.unmodifiableObservableList( segments);
+    }
+
+
+    public void addSegment(Segment s){
+        this.segments.add(s);
+    }
+
+    public void removeLast() {
+        if(!segments.isEmpty()){
+            segments.remove(segments.size() - 1);
+        }
+    }
+
+    public void removeOne(int index) {
+        segments.remove(index);
+    }
+
+    public void clear(){
+        segments.clear();
+    }
+
+}
