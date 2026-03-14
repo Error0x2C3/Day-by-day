8c5b5fe5fa820d19774a363db88153ac2fb0418f
Error0x2C3 <104620425+Error0x2C3@users.noreply.github.com>
2026-03-14 23:12:03 +0100

    Projet JavaFxÃ

---
+++ day-054/TurnLeft1.java
@@ -0,0 +1,39 @@
+package scratch.model;
+
+public class TurnLeft extends Action{
+    private Angle angle;
+    public TurnLeft(int initialAngle){
+        this.angle = new Angle(initialAngle);
+    }
+
+    // Par défautr, on bouge de 90°.
+    public TurnLeft(){
+        this(90);
+    }
+    public Angle getAngle(){
+        return  this.angle;
+    }
+    public void setAngle(int angle){
+        this.angle.setValue(angle);
+    }
+
+    @Override
+    public void execute(ExecutionContext ctx) {
+        //TODO
+    }
+
+    @Override
+    public ValidationResult validate() {
+        ValidationResult result = new ValidationResult();
+        if(!this.getAngle().isValid()){
+            result.addMessage(this.getAngle().getMessageError());
+            return result;
+        }
+        return result;
+    }
+
+    @Override
+    public String defaultLabel() {
+        return "Tourner à gauche de "+this.getAngle().getValue();
+    }
+}
