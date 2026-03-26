5f1c38b6738795c33c4c585c771af25af8a58cc0
Error0x2C3 <104620425+Error0x2C3@users.noreply.github.com>
2026-03-26 23:26:01 +0100

    ¨Projet web

--- day-051/Action.java
+++
@@ -1,8 +0,0 @@
-package scratch.model;
-
-public abstract class Action {
-
-    public abstract void execute(ExecutionContext ctx);
-    public abstract ValidationResult validate();
-    public abstract String defaultLabel();
-}
