8d5ab6cadc14d35dbb8fcfbbb1ed6259fe8dd20e
Error0x2C3 <104620425+Error0x2C3@users.noreply.github.com>
2026-03-26 23:26:33 +0100

    projet web

---
+++ day-061/view_sales.php
@@ -0,0 +1,21 @@
+<?php
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Profile</title>
+    <base href="<?= $web_root ?>">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.css" rel="stylesheet">
+    <link rel="stylesheet" href="css/navigation.css">
+    <link rel="stylesheet" href="css/profile.css">
+
+</head>
+
+<body class="bg-dark text-light">
+<main class="page-content p-3">
+
+</main>
+</body>
+</html>
