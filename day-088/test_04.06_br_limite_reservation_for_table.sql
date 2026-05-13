691cc0863375ada4ac90724d404ae71fb187d255
Error0x2C3 <104620425+Error0x2C3@users.noreply.github.com>
2026-05-13 02:00:07 +0200

    fix(test BR_06 for PRBDD): début

---
+++ day-088/test_04.06_br_limite_reservation_for_table.sql
@@ -0,0 +1,32 @@
+set search_path to public;
+
+-- cas negatif
+begin;
+do
+$test$
+    begin
+        RAISE NOTICE 'TEST BR-06 : Une table ne peut pas faire l''objet de deux réservations terminées ou confirmées pour une même date et un même service.';
+        /* INSERT INTO reservations (client, restaurant, datetime, number_of_guests, status,special_requests) */
+        INSERT INTO reservations (client, restaurant, datetime, number_of_guests, status)
+        VALUES (6, 1, '2026-05-15 19:00:00.000000', 2, 'pending');
+
+    end
+$test$;
+rollback;
+
+-- -- cas positif
+-- begin;
+-- do
+-- $test$
+--     DECLARE new_reservations_id int;
+--     begin
+--         RAISE NOTICE 'TEST BR-06 :le service peut etre admis car aucun service est a cette heure la ';
+--         INSERT INTO reservations ( client, restaurant, datetime, number_of_guests, status, special_requests)
+--         VALUES ( 6, 1, '2026-11-15 21:00:00.000000', 2, 'confirmed', 'visite')
+--         RETURNING id INTO new_reservations_id;
+--         INSERT INTO reservation_tables(reservation, "table") VALUES (new_reservations_id ,1);
+--
+--         set constraints all immediate;
+--     end
+-- $test$;
+-- rollback;
